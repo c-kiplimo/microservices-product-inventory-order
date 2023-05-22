6814db99238aacb36cff44d3d89f5f9c870359f2
c-kiplimo <kiplimocollins855@gmail.com>
2023-05-22 18:03:25 +0300

    setup inventory

---
+++ inventory-service/src/main/java/com/collicode/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.collicode.inventoryservice.repository;
+
+import com.collicode.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    List<Inventory> findBySkuCodeIn(List<String> skuCode);
+}
---
+++ inventory-service/src/main/java/com/collicode/inventoryservice/service/InventoryService.java
@@ -0,0 +1,33 @@
+package com.collicode.inventoryservice.service;
+
+import com.collicode.inventoryservice.dto.InventoryResponse;
+import com.collicode.inventoryservice.repository.InventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    @SneakyThrows
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        log.info("Checking Inventory");
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                        InventoryResponse.builder()
+                                .skuCode(inventory.getSkuCode())
+                                .isInStock(inventory.getQuantity() > 0)
+                                .build()
+                ).collect(Collectors.toList());
+    }
+}
